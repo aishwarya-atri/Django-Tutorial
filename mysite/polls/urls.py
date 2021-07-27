94cf69fb642856954be94b0dcb3b05c263839f4e
Aishwarya Mustoori <mustoori@usc.edu>
2021-07-27 12:37:42 -0700

    feat(Django Tutorial) : Forms and generic views
    Part 4 of the Django documentation which involves Forms and generic views
    
    [ ICTRAINING-782 ]

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -4,11 +4,17 @@ from . import views
 
 app_name = 'polls'
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.IndexView, name='index'),
 
-    path('specifics/<int:question_id>', views.detail, name='detail'),
+    path('specifics/<int:pk>', views.DetailView, name='detail'),
 
-    path('<int:question_id>/results/', views.results, name='results'),
+    path('<int:pk>/results/', views.ResultsView, name='results'),
+
+    # path('', views.index, name='index'),
+    #
+    # path('specifics/<int:question_id>', views.detail, name='detail'),
+    #
+    # path('<int:question_id>/results/', views.results, name='results'),
 
     path('<int:question_id>/vote/', views.vote, name='vote'),
 ]
