872dbe13ca0239e8daf80b6379c975a8cb12aaea
Aishwarya Mustoori <mustoori@usc.edu>
2021-07-28 10:15:31 -0700

    feat(Django Tutorial) : Admin Site
    Part 7 of Django Tutorial which shows how to make changes in admin site
    
    [ ICTRAINING-785 [

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -6,6 +6,7 @@ from django.db import models
 from django.utils import timezone
 import datetime
 
+from django.contrib import admin
 
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
@@ -17,6 +18,10 @@ class Question(models.Model):
     def was_published_recently(self):
         now = timezone.now()
         return now - datetime.timedelta(days=1) <= self.pub_date <= now
+
+    was_published_recently.boolean = True
+    was_published_recently.admin_order_field = 'pub_date'
+    was_published_recently.short_description = 'Published recently?'
 
 
 class Choice(models.Model):
